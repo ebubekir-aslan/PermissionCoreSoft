e8ffc406a8e149949d6c1f35a535f647c0ae9697
cizmaz <omerschone@hotmail.com>
2021-12-23 14:08:39 +0300

    Template oluşturuldu.

---
+++ PermissionCoreSoft/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PermissionCoreSoft.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
